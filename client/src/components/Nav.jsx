157b3e5171ce87e167b2ecac6ca4de59c61b36f2
Josh Levine <levine.joshuaadam@gmail.com>
2022-12-15 08:10:11 -0500

    nav formatting

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,25 +4,27 @@ const Nav = ({ user, handleSignout }) => {
   
   const publicOptions = (
     <div className="nav-options">
-      <Link to="/register">Register</Link>
-      <Link to="/signin">Sign In</Link>
+      <Link className="nav-text" to="/register">Register</Link>
+      <Link className="nav-text" to="/signin">Sign In</Link>
     </div>
   )
 
   const authOptions = (
-    <div className="nav-options">
-      <div>Signed in as: {user?.email}</div>
-      <Link to="/userhome">User Home</Link>
-      <Link to="/searchncbi">NCBI Search</Link>
-      <Link to="/accountdetails">Account Details</Link>
-      <Link onClick={handleSignout} to="/signin">Sign Out</Link>
-    </div>
+      <div className='authenticated-nav'>
+        <h3><span className='underline'>Signed in as</span> <br/>{user?.email}</h3>
+        <div className="nav-options">
+          <Link className="nav-text" to="/userhome">User Home</Link>
+          <Link className="nav-text" to="/searchncbi">NCBI Search</Link>
+          <Link className="nav-text" to="/accountdetails">Account Details</Link>
+          <Link className="nav-text" onClick={handleSignout} to="/signin">Sign Out</Link>
+        </div>
+      </div>
   )
 
   return (
       <nav className='nav-bar'>
-        <div className="welcome-container">
-          <h3 className="welcome-note">Seq Keeper</h3>          
+        <div>
+          <h1 className="app-title">Seq Keeper</h1>          
         </div>
         {user? authOptions : publicOptions}
       </nav>
